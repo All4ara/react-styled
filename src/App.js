f463794b07a0c3954a023bb33792f6a815ea5149
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-02-27 18:31:27 -0500

    listings-styled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Dropdown from './components/Dropdown';
 import { Hero } from './components/Hero';
 import InfoSection from './components/InfoSection';
+import ListingsSection from './components/ListingsSection';
 import { Navbar } from './components/Navbar'
 import { InfoData } from './data/InfoData';
 import { SliderData } from './data/SliderDate';
@@ -23,6 +24,7 @@ function App() {
       <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero slides={SliderData}/>
       <InfoSection {...InfoData}/>
+      <ListingsSection />
     </>
   );
 }
