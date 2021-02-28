d4e2d3e3ce5d67e396fd5dccac63641d89e901ff
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-02-27 19:20:19 -0500

    Footer-styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import Dropdown from './components/Dropdown';
 import Features from './components/Features';
+import Footer from './components/Footer';
 import { Hero } from './components/Hero';
 import InfoSection from './components/InfoSection';
 import ListingsSection from './components/ListingsSection';
@@ -28,6 +29,7 @@ function App() {
       <ListingsSection />
       <Features />
       <InfoSection {...InfoDataTwo}/>
+      <Footer />
     </>
   );
 }
