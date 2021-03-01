97bc44962cbf195185f4edbc28d0db3b9c3e887d
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-01 17:33:52 -0500

    contact form-styled

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Form from '../components/Form';
 import InfoSection from '../components/InfoSection'
 import { InfoData, InfoDataTwo } from '../data/InfoData';
 
 const Contact = () => {
     return (
         <>
+{/*         
         <InfoSection {...InfoData} />
-        <InfoSection {...InfoDataTwo} />
+        <InfoSection {...InfoDataTwo} /> */}
+        <Form />
         </>
     )
 }
