5cdb546d5dc315a5f0bfeac66a448d3ab393fcf5
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-04 18:50:43 -0500

    Iframe

--- src/pages/Homes.js
+++ src/pages/Homes.js
@@ -5,6 +5,7 @@ const Homes = () => {
     return (
         <>
             <ListingsSection />
+            <iframe src="https://matrix.bnymls.com/Matrix/public/IDX.aspx?idx=3c10134" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0"></iframe>
         </>
     )
 }
