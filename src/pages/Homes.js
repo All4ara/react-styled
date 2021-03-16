4d21e2331f91e71a3a20df25f69cfb9f281f7188
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-15 22:10:15 -0400

    fix

--- src/pages/Homes.js
+++ src/pages/Homes.js
@@ -4,8 +4,15 @@ import ListingsSection from '../components/ListingsSection'
 const Homes = () => {
     return (
         <>
-            <ListingsSection />
-            <iframe src="https://matrix.bnymls.com/Matrix/public/IDX.aspx?idx=3c10134" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0"></iframe>
+            {/* <ListingsSection /> */}
+            
+            <div className="container" >
+                <div className="row">
+                    <h1 style={{padding: "4rem 0", margin: 'auto'}}>Dream Home Search</h1>
+                    <iframe src="https://matrix.bnymls.com/Matrix/public/IDX.aspx?idx=3c10134" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0"></iframe>
+                </div>
+            </div>
+            
         </>
     )
 }
